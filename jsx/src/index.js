d577bed00e0acf3128c1e17c3853b4397608d8b8
Nock-zz <nick.morter@gmail.com>
2020-09-21 18:07:23 +0100

    Commit by nickmorter at 2020-09-18-07-23:

---
+++ jsx/src/index.js
@@ -0,0 +1,21 @@
+// import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// compose the JSX code 
+
+const App = () => {
+return <div>
+      <label className="label" htmlFor="name">
+         Enter Name:
+      </label>
+      <input type="text" id="name"/>
+      <button style={{backgroundColor:'blue', color:'white'}}>Submit</button>
+      </div>;
+};
+
+// render the result to the DOM
+ReactDOM.render(
+<App />,
+document.querySelector('#root')
+);
